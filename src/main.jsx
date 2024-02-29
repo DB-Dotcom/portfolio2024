7a12d100e9e75b4054c5887a4986ba967e39fc5b
WebDevDB <db.dci.student@gmail.com>
2024-02-29 11:24:25 +0100

    create error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider,} from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root.jsx";
+import ErrorPage from "./pages/error-page.jsx";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-  },
+    errorElement: <ErrorPage />,},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
